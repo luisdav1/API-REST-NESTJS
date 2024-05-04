74f7567010afec880e93946347ccf570b5290b67
luisdav1 <luisdav1@users.noreply.github.com>
2024-05-04 17:22:34 -0500

    Añadir types.ts

--- src/pizzas/pizzas.service.ts
+++ src/pizzas/pizzas.service.ts
@@ -1,25 +1,18 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUpdatePizza } from './dto/create-update.dto';
 import { PrismaService } from './prisma.service';
-
-export interface Pizza {
-    id:number,
-    nombre:string,
-    descripcion:string,
-    img: string,
-    like:number
-}
+import { TPizza } from './types/types';
 
 @Injectable()
 export class PizzasService {
     
     constructor(private prisma : PrismaService) {}
     
-    getPizzas(limite?:number) : Promise<Pizza[]> { 
+    async getPizzas(limite?:number) : Promise<TPizza[]> { 
         return  this.prisma.pizza.findMany({take:limite})
     }
 
-    async getPizza(id:number) : Promise<Pizza | NotFoundException>  {
+    async getPizza(id:number) : Promise<TPizza>  {
         const pizzaEncontrada = await this.prisma.pizza.findUnique({where: {id:id}})
         if (!pizzaEncontrada) {
             throw new NotFoundException(`No se encontro la pizza con el id ${id}`)
@@ -27,35 +20,37 @@ export class PizzasService {
         return pizzaEncontrada;
     } 
 
-    async createPizza(nuevaPizza : CreateUpdatePizza) : Promise<Pizza[]> {
+    async createPizza(nuevaPizza : CreateUpdatePizza) : Promise<TPizza[]> {
         await this.prisma.pizza.create({data:nuevaPizza})
         return this.prisma.pizza.findMany();
     }
 
-    async updatePizza(id: number, actualizarPizza : CreateUpdatePizza) : Promise<Pizza[] | NotFoundException> {
-        const result = await this.prisma.pizza.update({where:{id:id}, data:actualizarPizza})
-        if (!result) {
-            return new NotFoundException(`No se encontro la pizza con el id ${id}`)
+    async updatePizza(id: number, actualizarPizza : CreateUpdatePizza) : Promise<TPizza[]> {
+        const pizzaEncontrada = await this.prisma.pizza.findUnique({where: {id:id}})
+        if (!pizzaEncontrada) {
+            throw new NotFoundException(`No se encontro la pizza con el id ${id}`)
         } else { 
+            await this.prisma.pizza.update({where:{id:id}, data:actualizarPizza})
            return this.prisma.pizza.findMany()
         }
     }
 
-    async deletePizza(id:number) : Promise<Pizza[] | NotFoundException> {
-        const result = await this.prisma.pizza.delete({where:{id:id}})
-        if(!result) {
-            return new NotFoundException(`No se encontro la pizza con el id ${id}`);
+    async deletePizza(id:number) : Promise<TPizza[]> {8
+        const pizzaEncontrada = await this.prisma.pizza.findUnique({where: {id:id}})
+        if (!pizzaEncontrada) {
+            throw new NotFoundException(`No se encontro la pizza con el id ${id}`)
         } else {
+            await this.prisma.pizza.delete({where:{id:id}})
             return this.prisma.pizza.findMany()
         }
     }
 
-    async updateLike(id:number) :  Promise<Pizza[] | NotFoundException> {
-        const pizza = await this.prisma.pizza.findUnique({where: {id:id}}) 
-        if(!pizza) {
-            return new NotFoundException(`No se encontro la pizza con el id ${id}`);
+    async updateLike(id:number) :  Promise<TPizza[]> {
+        const pizzaEncontrada = await this.prisma.pizza.findUnique({where: {id:id}}) 
+        if(!pizzaEncontrada) {
+            throw new NotFoundException(`No se encontro la pizza con el id ${id}`);
         } else {
-            await this.prisma.pizza.update({where:{id:id},data:{like:pizza.like+1}})
+            await this.prisma.pizza.update({where:{id:id},data:{like:pizzaEncontrada.like+1}})
             return this.prisma.pizza.findMany()
         }
     }
---
+++ src/pizzas/types/types.ts
@@ -0,0 +1,8 @@
+
+export type TPizza =  {
+    id:number,
+    nombre:string,
+    descripcion:string,
+    img: string,
+    like:number
+}
